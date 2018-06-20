6eae47208e93b6a3064b684c63542cdd589b56eb
Alejandro Castillo <alexcc134@gmail.com>
2018-06-20 10:47:53 -0500

    Producto y ruta Lista

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import {AboutComponent, PortafolioComponent, ItemComponent} from "./components/i
 const app_routes: Routes = [
     {path: 'home',component: PortafolioComponent},
     {path: 'about',component: AboutComponent},
-    {path: 'item',component: ItemComponent},
+    {path: 'producto/:id',component: ItemComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
 
